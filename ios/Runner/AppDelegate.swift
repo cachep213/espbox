a176b5812f015d9ae63cb66c9d7747b86ca96c3b
thanhphongcpt <73149529+thanhphongcpt@users.noreply.github.com>
2023-08-20 21:41:32 +0700

    ios awsomen noti

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,9 @@
 import UIKit
 import Flutter
 import FirebaseCore
+import awesome_notifications
+import shared_preferences_ios
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,13 +11,14 @@ import FirebaseCore
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-     SwiftAwesomeNotificationsPlugin.setPluginRegistrantCallback { registry in          
+          GeneratedPluginRegistrant.register(with: self)
+          SwiftAwesomeNotificationsPlugin.setPluginRegistrantCallback { registry in          
           SwiftAwesomeNotificationsPlugin.register(
             with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)          
           FLTSharedPreferencesPlugin.register(
             with: registry.registrar(forPlugin: "io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin")!)
-      }
-    GeneratedPluginRegistrant.register(with: self)
+          }
+   
     FirebaseApp.configure()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
